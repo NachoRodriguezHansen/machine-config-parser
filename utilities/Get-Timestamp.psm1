536acf0af093cbb0c41a47f5fa914c2cc2b90346
rodrigui_muglobal <ignacio.rodriguez@es.multivac.com>
2025-11-12 18:01:32 +0100

    remove unnecessary comments

--- utilities/Get-Timestamp.psm1
+++ utilities/Get-Timestamp.psm1
@@ -1,9 +1,5 @@
 function Get-Timestamp {
-    <#
-    .SYNOPSIS
-    Returns current timestamp in format "HH:mm:ss.fff"
-    #>
-    return (Get-Date -Format "HH:mm:ss.fff") + " - "
+    return (Get-Date -Format "HH:mm:ss.fff") + " -"
 }
 
 Export-ModuleMember -Function Get-Timestamp
